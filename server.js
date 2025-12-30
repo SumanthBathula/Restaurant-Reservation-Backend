706428463a9f845fc5fd4bc521ac3bff0cdae88c
SumanthBathula <sumanthbittu16@gmail.com>
2025-12-30 15:27:43 +0530

    fixed errors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/restaurant-reservation', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://sumanth:tpE9lqoZPfrTBI6e@sumanth.afweq.mongodb.net/?retryWrites=true&w=majority&appName=sumanth/restaurant-reservation', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
